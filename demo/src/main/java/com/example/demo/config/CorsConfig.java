83dad17f83888b9578cde3ad56961a82651a482e
DESKTOP-DRVOVRL\user <jegyeong25@gmail.com>
2025-10-16 18:50:40 +0900

    docker file

--- demo/src/main/java/com/example/demo/config/CorsConfig.java
+++ demo/src/main/java/com/example/demo/config/CorsConfig.java
@@ -11,7 +11,7 @@ public class CorsConfig implements WebMvcConfigurer{
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/api/**")
-				.allowedOrigins("http://localhost:3000")
+				.allowedOriginPatterns("*")  // 모든 도메인 허용
 				.allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
 				.allowedHeaders("*")
 				.allowCredentials(true)
